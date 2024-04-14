8906f85a8e4575e8c958f9d095c990714030724d
Jay Patel <jaykrinapatel@gmail.com>
2024-04-14 07:17:47 -0500

    Adding new branch

--- myappvs/src/main/java/com/project/myappvs/MyappvsApplication.java
+++ myappvs/src/main/java/com/project/myappvs/MyappvsApplication.java
@@ -2,6 +2,10 @@ package com.project.myappvs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MyappvsApplication {
@@ -9,6 +13,15 @@ public class MyappvsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MyappvsApplication.class, args);
 		System.out.println("Started...");
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins("*");
+			}
+		};
 	}
 
 }
