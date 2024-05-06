421219291b1800e746aee72ab7f82325141eb505
Harsha <greddy2@depaul.edu>
2024-05-06 01:53:50 -0500

    Add appointment package and related classes

---
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/appointment/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * The appointment entity
+ * <li>Many to one relationship between appointment and student
+ * <li>Many to one relationship between appointment and advisor
+ * 
+ * @author Guntaka Satish Harshavardhan Reddy
+ */
+package com.CourseManagementSystem.myappvs.appointment;
