bab141b504c47837864d9f760673287e3b820ebe
Atif Siddiqui <asiddi27@depaul.edu>
2024-05-06 22:48:12 -0500

    Fixed package.info file

--- myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/package-info.java
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/package-info.java
@@ -1,10 +1,10 @@
 /**
- * <b>Show case all that I learned during the course to demonstrate to my potential employer
- * <ul>
- * <li>Unit Tests
- * <li>Documentations on features that were not covered in class
- * <li>Repository with additional finders
- * </ul>
+ * /**
+ *  * The Equipment Entity
+ *  * <li> Mapped one to many
+ *  * The Rental Entity
+ *  * <li> Mapped one to one</li>
+ *  * <li> Can return all rentals</li>
  * @author Atif Siddiqui
  */
 
