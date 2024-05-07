c698e2f6a2dc806e76894f95049830b391094667
Shivangi Patel <patelshivangioct@gmail.com>
2024-05-07 16:10:52 -0500

    Resolved conflicts

---
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/EquipmentService.java
@@ -0,0 +1,23 @@
+package com.CourseManagementSystem.myappvs.rentals;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Service
+public class EquipmentService {
+    @Autowired
+    private EquipmentRepository equipmentRepository;
+
+    public List<Equipment> findAllEquipment() {
+        return null;
+    }
+
+
+}
