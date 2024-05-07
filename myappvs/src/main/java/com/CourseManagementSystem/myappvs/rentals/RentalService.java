fa56f9c798945e51bc4d3d7d1a49090dc76fd7c9
Atif Siddiqui <asiddi27@depaul.edu>
2024-05-06 20:05:11 -0500

    Adding preliminary repository for rentals

---
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/RentalService.java
@@ -0,0 +1,18 @@
+package com.CourseManagementSystem.myappvs.rentals;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class RentalService {
+
+    @Autowired
+    private RentalRepository rentalRepository;
+
+    public List<Rental> findAllRentals() {
+        return rentalRepository.findAll();
+    }
+
+    // Additional methods to handle rental logic
+}
+
