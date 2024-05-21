9637fe6960b751e7846050e8f52bec30d1e3f6c8
Atif Siddiqui <asiddi27@depaul.edu>
2024-05-20 21:12:04 -0500

    fixing up my code to avoid overwriting others' code

--- myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/package-info.java
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/rentals/package-info.java
@@ -7,6 +7,7 @@
  * Will implement service to take out rentals and have them tracked via the rental and Equipment tables
  * The rental tables currently are tied to user account info, and are only displayed ot logged-in users
  * and keep track of the rentals they have taken out.
+ * Everything in the rentals package is a work in progress and is being worked on by me.
  * *
  * Previous milestone:
  *  * The Equipment Entity
