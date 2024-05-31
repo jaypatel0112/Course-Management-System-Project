46ce72c7cb59b2c65da02fed3065c80d618c3f11
Jay Patel <jaykrinapatel@gmail.com>
2024-05-31 15:20:59 -0500

    Added the CRUD features in event section.

--- myappvs/src/main/java/com/CourseManagementSystem/myappvs/event/EventController.java
+++ myappvs/src/main/java/com/CourseManagementSystem/myappvs/event/EventController.java
@@ -1,32 +1,59 @@
 package com.CourseManagementSystem.myappvs.event;
 
-import java.util.Date;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @RestController
+@RequestMapping("/events")
 public class EventController {
 
     @Autowired
     private Eventrepo eventRepository;
 
-    @GetMapping("/events/{date}")
+    // Get events by date
+    @GetMapping("/{date}")
     public List<Event> getEventsByDate(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
         return eventRepository.findByDate(date);
     }
 
-    @PostMapping("/events")
-    public Event createEvent(@RequestBody Event event) {
+    // Create a new event
+    @PostMapping
+    public Event createEvent(@Validated @RequestBody Event event) {
         return eventRepository.save(event);
     }
 
-    @GetMapping("/events")
+    // Update an event
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable String id, @Validated @RequestBody Event eventDetails) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow();
+
+        event.setDate(eventDetails.getDate());
+        event.setEventName(eventDetails.getEventName());
+        event.setOrganizerName(eventDetails.getOrganizerName());
+
+        final Event updatedEvent = eventRepository.save(event);
+        return ResponseEntity.ok(updatedEvent);
+    }
+
+    // Delete an event
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteEvent(@PathVariable String id) {
+        Event event = eventRepository.findById(id)
+                .orElseThrow();
+
+        eventRepository.delete(event);
+        return ResponseEntity.ok().build();
+    }
+
+    // Get all events
+    @GetMapping
     public List<Event> getAllEvents() {
         return eventRepository.findAll();
     }
