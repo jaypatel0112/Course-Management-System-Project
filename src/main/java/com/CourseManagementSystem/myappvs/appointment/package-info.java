631880ba5b8c6adce7383826885784770ec99f06
Harsha <greddy2@depaul.edu>
2024-06-11 19:26:23 -0500

    Update package-info

--- src/main/java/com/CourseManagementSystem/myappvs/appointment/package-info.java
+++ src/main/java/com/CourseManagementSystem/myappvs/appointment/package-info.java
@@ -11,6 +11,17 @@
  * Repository class:
  * To find appointments based on the fields in the entity. Extra finders such as
  * find all appointments by advisor id have also been implemented as an abstract method.
+ * 
+ *Appointment Service 
+ The service layer of the appointment handles the CRUD operations for appointments.
+ *
+ * Appointment Controller:
+ * A rest controller to manipulate the view. Endpoint serves both the student and admin users.
+ * 
+ * Apppointment Request class:
+ * A data transfer object (DTO) to share appointment requests between the view and controller. A simple data object is
+ * easy to create on the view side and furhtermore, the controller doesn't have to give up the references of the corrseponding
+ * entities.
  * @author Guntaka Satish Harshavardhan Reddy
  */
 package com.CourseManagementSystem.myappvs.appointment;
